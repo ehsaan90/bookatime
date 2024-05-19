b5a980e215bde59968bad13f9bd09e0e1bc7063e
ehsaan90 <ehsaan90@outlook.com>
2024-05-19 17:46:08 +0500

    first commit

---
+++ User_web_version/src/environments/environment.prod.ts
@@ -0,0 +1,28 @@
+/*
+  Authors : initappz (Rahul Jograna)
+  Website : https://initappz.com/
+  App Name : Ultimate Salon Full App Flutter
+  This App Template Source code is licensed as per the
+  terms found in the Website https://initappz.com/license
+  Copyright and Good Faith Purchasers © 2022-present initappz.
+*/
+export const environment = {
+  production: true,
+  baseURL: 'https://book-a-time.com/api/public/api/',
+  mediaURL: 'https://book-a-time.com/api/public/storage/images/',
+  app_name: 'Ultimate Salon',
+  websiteURL: 'https://book-a-time.com',
+  websiteName: 'book-a-time.com',
+  default_country_code: '92',
+  firebase: {
+    apiKey: "creds",
+    authDomain: "creds",
+    databaseURL: "creds",
+    projectId: "creds",
+    storageBucket: "creds",
+    messagingSenderId: "creds",
+    appId: "creds",
+    measurementId: "creds"
+  },
+  googleMapsKeys: ''
+};
